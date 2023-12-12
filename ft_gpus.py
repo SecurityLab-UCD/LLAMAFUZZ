45008b02e13d2efdec22eb30aad7ee07d2c3152c
hxxzhang <zzhhxx000@gmail.com>
2023-12-12 11:24:20 -0800

    fix not in one device bug

--- ft_gpus.py
+++ ft_gpus.py
@@ -1,4 +1,4 @@
-# train from scratch
+# Trianed from checkpoint
 import os
 from dataclasses import dataclass, field
 from typing import Optional
@@ -14,16 +14,21 @@ from transformers import (
     HfArgumentParser,
     TrainingArguments,
 )
-from trl import SFTTrainer
 
-new_model = "llama-2-7b-structured-jpg-hex-40"
+from trl import SFTTrainer
+from trl.trainer import ConstantLengthDataset
+
+based_model_name = "llama-2-7b-structured-jpg-hex-40"
+new_model = "llama-2-7b-structured-jpg-png-hex"
+
+device = Accelerator().local_process_index
 
 
 def formatting_prompts_func_with_text(examples):
     output_texts = []
 
     for i in range(len(examples["context"])):
-        text = f"### Input: ```Generate a JPG example in hex format. Make sure the example is complete and valid. Only return the solution, no other words. Here is the text description you can refer A JPG image is represented as a sequence of segments where each segment begins with a marker. Each marker starts with 0xFF byte followed by marker flag to represent the type of marker. The payload followed by marker is different as per marker type. Common JPEG marker types are as listed below:Short Name Bytes	Payload	Name Comments```\n```SOI	0xFF, 0xD8 none Start of Image```\n ```S0F0 0xFF, 0xC0	variable size Start of Frame``` \n```S0F2 0xFF, 0xC2	variable size Start fo Frame```\n```DHT	0xFF, 0xC4	variable size Define Huffman Tables```\n```DQT	0xFF, 0xDB	variable size Define Quantization Table(s)```\n```DRI	0xFF, 0xDD	4 bytes	Define Restart Interval```\n```SOS	0xFF, 0xDA	variable size	Start Of Scan```\n```RSTn 0xFF, 0xD//n//(//n//#0..7) none Restart```\n```APPn 0xFF, 0xE//n// variable size Application specific```\n```COM	0xFF, 0xFE variable size Comment```\n```EOI	0xFF, 0xD9	none End Of Image```\n```Within the entropy-coded data, after any 0xFF byte, a 0x00 byte is inserted by the encoder before the next byte, so that there does not appear to be a marker where none is intended, preventing framing errors. Decoders must skip this 0x00 byte. This technique, called byte stuffing (see JPEG specification section F.1.2.3), is only applied to the entropy-coded data, not to marker payload data. Note however that entropy-coded data has a few markers of its own; specifically the Reset markers (0xD0 through 0xD7), which are used to isolate independent chunks of entropy-coded data to allow parallel decoding, and encoders are free to insert these Reset markers at regular intervals (although not all encoders do this).```\n ### Output: {examples['context'][i]}"
+        text = f"### Input: ```Generate a different PNG example in hex format. Make sure the example is complete and valid. Only return the solution, no other words. Here is the text description you can refer The Examples Structure of a very simple PNG file 89 50 4E 47 0D 0A 1A 0A```\n```Contents of a minimal PNG file representing one red pixel Hex As characters 89 50 4E 47 0D 0A 1A 0A 00 00 00 0D 49 48 44 52```\n```00 00 00 01 00 00 00 01 08 02 00 00 00 90 77 53```\n```DE 00 00 00 0C 49 44 41 54 08 D7 63 F8 CF C0 00```\n```00 03 01 01 00 18 DD 8D B0 00 00 00 00 49 45 4E```\n```44 AE 42 60 82```\n```IHDR Chunk Offset into chunk	Hex Value	Decimal Value	Text	Meaning```\n```0x0D	13		IHDR chunk has 13 bytes of content```\n```0x49484452		IHDR	Identifies a Header chunk```\n```0x01	1		Image is 1 pixel wide```\n```0x01	1		Image is 1 pixel high```\n```0x08	8		8 bits per pixel (per channel)```\n```0x02	2		Color type 2 (RGB/truecolor)```\n```0x00	0		Compression method 0 (only accepted value)```0x00	0		Filter method 0 (only accepted value)```\n```0x00	0		Not interlaced```\n```0x907753DE			CRC of chunk's type and content (but not length)```\n```IDAT Chunk Offset into chunk	Hex Value	Meaning```\n```0x0C	IDAT chunk has 12 bytes of content```\n```0x49444154	Identifies a Data chunk```\n```0x08	DEFLATE compression method using a 256-byte window[44]```\n```0xD7	ZLIB FCHECK value, no dictionary used, maximum compression algorithm```\n```0x63F8CFC00000	A compressed DEFLATE block using the static Huffman code that decodes to 0x00 0xFF 0x00 0x00```\n```0x03010100	The ZLIB check value: the Adler32 checksum of the uncompressed data```\n```0x18DD8DB0	CRC of chunk's type and content (but not length)```\n ### Output: {examples['context'][i]}"
         output_texts.append(text)
 
     return output_texts
@@ -33,7 +38,7 @@ def formatting_prompts_func(examples):
     output_texts = []
 
     for i in range(len(examples["context"])):
-        text = f"### Input: ```Generate a JPG example in hex format. Make sure the example is complete and valid. Only return the solution, no other words.```\n ### Output: {examples['context'][i]}"
+        text = f"### Input: ```Generate a different PNG example in hex format. Make sure the example is complete and valid. Only return the solution, no other words.```\n ### Output: {examples['context'][i]}"
         output_texts.append(text)
 
     return output_texts
@@ -45,13 +50,13 @@ class ScriptArguments:
         default="meta-llama/Llama-2-7b-chat-hf", metadata={"help": "the model name"}
     )
     num_train_epochs: Optional[int] = field(
-        default=40, metadata={"help": "Number of training epochs"}
+        default=200, metadata={"help": "Number of training epochs"}
     )
     per_device_train_batch_size: Optional[int] = field(
-        default=9, metadata={"help": "the per device train batch size"}
+        default=1, metadata={"help": "the per device train batch size"}
     )
     seq_length: Optional[int] = field(
-        default=2048, metadata={"help": "the sequence length"}
+        default=1900, metadata={"help": "the sequence length"}
     )
     max_steps: Optional[int] = field(
         default=-1,
@@ -64,18 +69,17 @@ class ScriptArguments:
         default=0, metadata={"help": "the saving frequency"}
     )
     gradient_accumulation_steps: Optional[int] = field(
-        default=2, metadata={"help": "the gradient accumulation steps"}
+        default=1, metadata={"help": "the gradient accumulation steps"}
     )
     gradient_checkpointing: Optional[bool] = field(
         default=True, metadata={"help": "whether to use gradient checkpointing"}
     )
     group_by_length: Optional[bool] = field(
-        default=True, metadata={"help": "whether to group by length"}
+        default=False, metadata={"help": "whether to group by length"}
     )
     packing: Optional[bool] = field(
-        default=False, metadata={"help": "whether to use packing for SFTTrainer"}
+        default=True, metadata={"help": "whether to use packing for SFTTrainer"}
     )
-
     lora_alpha: Optional[float] = field(
         default=16, metadata={"help": "the lora alpha parameter"}
     )
@@ -110,25 +114,6 @@ class ScriptArguments:
 parser = HfArgumentParser(ScriptArguments)
 script_args = parser.parse_args_into_dataclasses()[0]
 
-if script_args.group_by_length and script_args.packing:
-    raise ValueError("Cannot use both packing and group by length")
-
-
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.bfloat16,
-)
-
-base_model = AutoModelForCausalLM.from_pretrained(
-    script_args.model_name,
-    quantization_config=bnb_config,
-    device_map={"": Accelerator().local_process_index},
-    trust_remote_code=True,
-    use_auth_token=True,
-)
-base_model.config.use_cache = False
-
 peft_config = LoraConfig(
     r=script_args.lora_r,
     lora_alpha=script_args.lora_alpha,
@@ -138,12 +123,8 @@ peft_config = LoraConfig(
     task_type="CAUSAL_LM",
 )
 
-tokenizer = AutoTokenizer.from_pretrained(
-    script_args.model_name, trust_remote_code=True, padding=True
-)
-tokenizer.pad_token = tokenizer.eos_token
-tokenizer.padding_side = "right"  # Fix weird overflow issue with fp16 training
-
+if script_args.group_by_length and script_args.packing:
+    raise ValueError("Cannot use both packing and group by length")
 
 training_args = TrainingArguments(
     output_dir=script_args.output_dir,
@@ -167,26 +148,89 @@ training_args = TrainingArguments(
 )
 
 
-# Load dataset (you can process it here)
-dataset = load_dataset(
-    "csv",
-    data_files="../dataset/cleaneddata/jpg_hex.csv",
-    split="train",
-    delimiter="/n",
+def formatting_prompts_func(examples):
+    return f"### Input: ```Generate a different PNG example in hex format. Make sure the example is complete and valid. Only return the solution, no other words.```\n ### Output: {examples['context']}"
+    output_texts = []
+
+    for i in range(len(examples["context"])):
+        text = f"### Input: ```Generate a different PNG example in hex format. Make sure the example is complete and valid. Only return the solution, no other words.```\n ### Output: {examples['context'][i]}"
+        output_texts.append(text)
+
+    return output_texts
+
+
+def chars_token_ratio(dataset, tokenizer, nb_examples=400):
+    """
+    Estimate the average number of characters per token in the dataset.
+    """
+    total_characters, total_tokens = 0, 0
+    for _, example in tqdm(zip(range(nb_examples), iter(dataset)), total=nb_examples):
+        text = formatting_prompts_func(example)
+        total_characters += len(text)
+        if tokenizer.is_fast:
+            total_tokens += len(tokenizer(text).tokens())
+        else:
+            total_tokens += len(tokenizer.tokenize(text))
+
+    return total_characters / total_tokens
+
+
+def create_datset(tokenizer, data_file_path):
+    dataset = load_dataset(
+        "csv",
+        data_files=data_file_path,
+        split="train",
+        delimiter="/n",
+    )
+    chars_per_token = chars_token_ratio(dataset, tokenizer)
+    dataset = ConstantLengthDataset(
+        tokenizer,
+        dataset,
+        formatting_func=formatting_prompts_func,
+        infinite=True,
+        seq_length=script_args.seq_length,
+        chars_per_token=chars_per_token,
+    )
+    return dataset
+
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    torch_dtype=torch.bfloat16,
+    bnb_4bit_compute_dtype=torch.bfloat16,
+    device_map=device,
 )
+
+base_model = AutoModelForCausalLM.from_pretrained(
+    script_args.model_name,
+    quantization_config=bnb_config,
+    device_map={"": Accelerator().local_process_index},
+    trust_remote_code=True,
+    use_auth_token=True,
+)
+base_model.config.use_cache = False
+
+tokenizer = AutoTokenizer.from_pretrained(
+    "./" + based_model_name, trust_remote_code=True, padding=True
+)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"  # Fix weird overflow issue with fp16 training
+
+# Load dataset (you can process it here)
+dataset = create_datset(tokenizer, "../dataset/cleaneddata/png_hex.csv")
+
 
 trainer = SFTTrainer(
     model=base_model,
     train_dataset=dataset,
-    formatting_func=formatting_prompts_func,
     peft_config=peft_config,
-    # packing=script_args.packing,
+    packing=script_args.packing,
     max_seq_length=script_args.seq_length,
     tokenizer=tokenizer,
     args=training_args,
 )
-trainer.train()
-trainer.save_model(script_args.output_dir)
+trainer.train(resume_from_checkpoint=script_args.output_dir + "/final_checkpoint")
 
 output_dir = os.path.join(script_args.output_dir, "final_checkpoint")
 trainer.save_model(new_model)
