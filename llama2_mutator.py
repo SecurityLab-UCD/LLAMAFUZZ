167fc3669b7960142096b1872d0666062f8573b3
hxxzhang <zzhhxx000@gmail.com>
2024-02-23 12:20:30 -0800

    fix load in 4 bits

--- llama2_mutator.py
+++ llama2_mutator.py
@@ -219,7 +219,6 @@ def main():
         token=access_token,
         torch_dtype=torch.bfloat16,
         quantization_config=BitsAndBytesConfig(
-            load_in_4bit=True,
             bnb_4bit_quant_type="nf4",
             bnb_4bit_compute_dtype=torch.bfloat16,
             bnb_4bit_use_double_quant=True,
