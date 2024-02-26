ffe3f05dfcdf2404c936bfb9c73c294bce46bd40
hxxzhang <zzhhxx000@gmail.com>
2024-02-25 22:07:29 -0800

    remove late start

--- llama2_mutator.py
+++ llama2_mutator.py
@@ -272,5 +272,5 @@ if __name__ == "__main__":
     # if accelerator.is_main_process:
     # t2 = threading.Thread(target=reward_thread, args=())
     # t2.start()
-    time.sleep(7200)
+    # time.sleep(7200)
     main()
