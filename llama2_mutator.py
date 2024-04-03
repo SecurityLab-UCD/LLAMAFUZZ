8ee4c1573b2724504acba749b665967518bc91b6
hxxzhang <zzhhxx000@gmail.com>
2024-04-03 13:20:31 -0700

    print msg len

--- llama2_mutator.py
+++ llama2_mutator.py
@@ -100,7 +100,7 @@ def mq_thread():
                 if len(seed) > (2045 - len(send_msg)):
                     seed = seed[: (2045 - len(send_msg))]
                     send_msg = send_msg + seed.encode("utf-8")
-                print(":::send seed", send_msg[:15])
+                print(":::send seed", len(send_msg), send_msg[:15])
                 if not args.if_text:
                     mq.send(
                         send_msg,
